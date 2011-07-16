908722840ce10520e02e81308424908068dd0aba
Kerry Sainsbury <kerry@fidelma.com>
2011-07-16 19:01:24 +1200

    Fixed up some unit tests Expanded the metadata lists

--- src/com/fidelma/salesforce/misc/Deployment.java
+++ src/com/fidelma/salesforce/misc/Deployment.java
@@ -54,39 +54,66 @@ public class Deployment {
 
     // http://www.salesforce.com/us/developer/docs/api_meta/Content/file_based.htm#component_folders_title
     private String determineDirectoryName(String typeName) throws Exception {
+        if (typeName.equalsIgnoreCase("ActionOverride")) return "objects";
         if (typeName.equalsIgnoreCase("ApexClass")) return "classes";
+        if (typeName.equalsIgnoreCase("ArticleType")) return "objects";
         if (typeName.equalsIgnoreCase("ApexComponent")) return "components";
         if (typeName.equalsIgnoreCase("ApexPage")) return "pages";
         if (typeName.equalsIgnoreCase("ApexTrigger")) return "triggers";
+        if (typeName.equalsIgnoreCase("BusinessProcess")) return "objects";
+        if (typeName.equalsIgnoreCase("CustomApplication")) return "applications";
         if (typeName.equalsIgnoreCase("CustomField")) return "objects";
+        if (typeName.equalsIgnoreCase("CustomLabel")) return "labels";
         if (typeName.equalsIgnoreCase("CustomObject")) return "objects";
+        if (typeName.equalsIgnoreCase("CustomObjectTranslation")) return "objectTranslations";
+        if (typeName.equalsIgnoreCase("CustomPageWebLink")) return "weblinks";
+        if (typeName.equalsIgnoreCase("CustomSite")) return "sites";
+        if (typeName.equalsIgnoreCase("CustomTab")) return "tabs";
+        if (typeName.equalsIgnoreCase("Dashboard")) return "dashboards";
+        if (typeName.equalsIgnoreCase("DataCategoryGroup")) return "datacategorygroups";
+        if (typeName.equalsIgnoreCase("Document")) return "document";
+        if (typeName.equalsIgnoreCase("EmailTemplate")) return "email";
+        if (typeName.equalsIgnoreCase("EntitlementTemplate")) return "entitlementTemplates";
+        if (typeName.equalsIgnoreCase("FieldSet")) return "objects";
+        if (typeName.equalsIgnoreCase("HomePageComponent")) return "homePageComponents";
+        if (typeName.equalsIgnoreCase("HomePageLayout")) return "homePageLayouts";
+        if (typeName.equalsIgnoreCase("Layout")) return "layouts";
+        if (typeName.equalsIgnoreCase("Letterhead")) return "letterhead";
+        if (typeName.equalsIgnoreCase("ListView")) return "objects";
+        if (typeName.equalsIgnoreCase("NamedFilter")) return "objects";
+        if (typeName.equalsIgnoreCase("PermissionSet")) return "permissionsets";
+        if (typeName.equalsIgnoreCase("Portal")) return "portals";
         if (typeName.equalsIgnoreCase("Profile")) return "profiles";
         if (typeName.equalsIgnoreCase("RecordType")) return "recordtypes";
+        if (typeName.equalsIgnoreCase("RemoteSiteSetting")) return "remoteSiteSettings";
+        if (typeName.equalsIgnoreCase("Report")) return "reports";
+        if (typeName.equalsIgnoreCase("ReportType")) return "reportTypes";
+        if (typeName.equalsIgnoreCase("Scontrol")) return "scontrols";
+        if (typeName.equalsIgnoreCase("SharingReason")) return "objects";
+        if (typeName.equalsIgnoreCase("SharingRecalculation")) return "objects";
         if (typeName.equalsIgnoreCase("StaticResource")) return "staticresources";
+        if (typeName.equalsIgnoreCase("Translations")) return "translations";
+        if (typeName.equalsIgnoreCase("ValidationRule")) return "objects";
+        if (typeName.equalsIgnoreCase("Weblink")) return "objects";
         if (typeName.toUpperCase().startsWith("WORKFLOW")) return "workflows";
-        if (typeName.equalsIgnoreCase("Layout")) return "layouts";
-        if (typeName.equalsIgnoreCase("CustomLabel")) return "labels";
 
-        if (typeName.equalsIgnoreCase("ActionOverride")) return "objects";
-        throw new Exception("Unsupported type: " + typeName);
+        System.err.println("WARNING: Unknown type " + typeName + " -- guessing at 'objects'");
+
+        return "objects";
     }
 
-    private String determineFileSuffix(String typeName) throws Exception {
-        if (typeName.equalsIgnoreCase("ApexClass")) return "cls";
-        if (typeName.equalsIgnoreCase("ApexComponent")) return "component";
-        if (typeName.equalsIgnoreCase("ApexPage")) return "page";
-        if (typeName.equalsIgnoreCase("ApexTrigger")) return "trigger";
-        if (typeName.equalsIgnoreCase("CustomField")) return "object";
-        if (typeName.equalsIgnoreCase("CustomObject")) return "object";
-        if (typeName.equalsIgnoreCase("Profile")) return "profile";
-        if (typeName.equalsIgnoreCase("RecordType")) return "recordtype";
-        if (typeName.equalsIgnoreCase("StaticResource")) return "resource";
-        if (typeName.toUpperCase().startsWith("WORKFLOW")) return "workflows";
-        if (typeName.equalsIgnoreCase("Layout")) return "layout";
-        if (typeName.equalsIgnoreCase("CustomLabel")) return "labels";
-        if (typeName.equalsIgnoreCase("ActionOverride")) return "objects";
+     private String determineFileSuffix(String typeName) throws Exception {
+        String directoryName = determineDirectoryName(typeName);
+        String suffix;
+        if (directoryName.equals("classes")) {
+            suffix = "cls";
 
-        throw new Exception("Unsupported type: " + typeName);
+        } else if (directoryName.equalsIgnoreCase("StaticResource")) {
+            suffix = "resource";
+        } else {
+            suffix = directoryName.substring(0, directoryName.length()-1);
+        }
+        return suffix;
     }
 
 
